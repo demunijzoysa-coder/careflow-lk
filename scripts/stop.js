5accbaa3d5c12cfcc10980133c298e3ee65b9cca
demunijzoysa-coder <demuni.j.zoysa@gmail.com>
2026-01-06 20:30:20 +0530

    chore: setup root workspace scripts and tooling

---
+++ scripts/stop.js
@@ -0,0 +1,4 @@
+// Simple cross-platform placeholder.
+// In later steps we will upgrade this to stop multiple processes cleanly.
+console.log("Stop: nothing to stop yet. (We will wire this once dev servers exist.)");
+process.exit(0);
